564920bf12819d4109b207b3ce37ed151d19b792
mailen912 <62101563+mailen912@users.noreply.github.com>
2020-05-27 17:38:08 -0300

    Update ajustes.py

--- ajustes.py
+++ ajustes.py
@@ -6,7 +6,7 @@ def correr():
     #layout de configuracion
     letras=list(string.ascii_uppercase)
     letras.insert(14,'Ñ')
-
+    dificultad={'Facil':180,'Medio':120,'Dificil':60}
     lista_col1=[]
     for x in letras[0:14]:
         lista_col1.append([sg.Text(x),sg.InputText(size=(4,1)),sg.InputText(size=(4,1))])
@@ -15,15 +15,17 @@ def correr():
         lista_col2.append([sg.Text(x),sg.InputText(size=(4,1)),sg.InputText(size=(4,1))])
 
     layout_config=[[sg.Text('Dificultad:'),sg.InputCombo(values=('Facil','Medio','Dificil'))],
-    [sg.Text('Tiempo en segundos: )'),sg.InputText()],
+    [sg.Text('Tiempo en segundos:               ')],
     [sg.Text('Cantidades de letras y puntos:')],
     [sg.Column(lista_col1),sg.Column(lista_col2)],
     [sg.Button('Guardar'),sg.Button('Cerrar')]
     ]
 
     windowconf = sg.Window('Configuracion').Layout(layout_config)
-    evento = windowconf.Read()
+    
     while True:
+        evento,valores = windowconf.Read()
+        print(evento,valores)
         if evento=='Guardar':
             archivo=open('config.txt','w')
             json.dump(evento[1],archivo)
