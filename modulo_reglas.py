d08fb60c58a8f0067b471eaf8550d5041ae44a9a
gomezlucia <62036346+gomezlucia@users.noreply.github.com>
2020-06-15 20:23:25 -0300

    Create modulo_reglas.py

---
+++ modulo_reglas.py
@@ -0,0 +1,21 @@
+import PySimpleGUI as sg
+
+
+def imprimir_reglas():
+
+    '''esta funcion abre el archivo donde se encuentran las reglas del juego y las imprime en una ventana nueva
+    tiene un boton para volver al tablero'''
+
+    reglas=open('arch_reglas.txt','r')
+    imprimir= lambda linea: reglas.read()
+
+    layout = [[sg.Text(reglas.read())],
+            [sg.Button('Volver',button_color=('white','black'),key='volver')]
+            ]
+    window = sg.Window('Reglas',layout)
+    event, values = window.Read()
+#    while true:
+        
+#    if event == 'volver':
+#        break
+#imprimir_reglas()
