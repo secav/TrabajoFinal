0b9472c7c3f49e3a2e91677ba2f188c14a63f727
gomezlucia <62036346+gomezlucia@users.noreply.github.com>
2020-07-13 16:44:26 -0300

    Update modulo_reglas.py

--- funciones_comunes/modulo_reglas.py
+++ funciones_comunes/modulo_reglas.py
@@ -5,8 +5,14 @@ def imprimir_reglas():
 
     '''esta funcion abre el archivo donde se encuentran las reglas del juego y las imprime en una ventana nueva
     tiene un boton para volver al tablero'''
+    def aviso():
+        '''mensaje que aparece si no se encuantran los archivos necesarios para abrir la ventana'''
+        sg.popup('No se ha encontrado el archivo correspondiente para abrir esta ventana')
 
-    reglas=open('./datos/arch_reglas.txt','r', encoding="utf8")
+    try:
+        reglas=open('./datos/arch_reglas.txt','r', encoding="utf8")
+    except FileNotFoundError:
+        aviso()
 
     layout = [[sg.Text(reglas.read(), size=(100,30))],
             [sg.Button('Volver',button_color=('white','black'),key='volver')]
